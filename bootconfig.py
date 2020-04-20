1846f39de2a5695a7b4fc8a53fc5b81b7327ee31
Christian Schneemann <schneemann@b1-systems.de>
2020-04-20 09:25:10 +0200

    initial commit

---
+++ bootconfig.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import yaml
+import cgi
+#import cgitb;cgitb.enable()
+
+configfile = "./bootconfig.yaml"
+
+def translate(s):
+    s = s.replace("%3A;", ":")
+    return s
+
+def get_bootparam(obj, conf_type):
+    try:
+        config[obj][conf_type]
+    except KeyError:
+        obj = "default"
+    return config[obj][conf_type]
+
+with open(configfile, 'r') as yamlobjects:
+    config = yaml.load(yamlobjects, Loader=yaml.FullLoader)
+
+arguments = cgi.FieldStorage()
+if "boot" not in arguments:
+  BOOT = "default"
+else:
+  BOOT = translate(arguments["boot"].value).lower()
+
+kernel = get_bootparam(BOOT, "kernel")
+initrd = get_bootparam(BOOT, "initrd")
+parameter = get_bootparam(BOOT, "parameter")
+
+print("Content-type: text/plain\n")
+print("#!ipxe\n");
+print("#booting option {0:s}\n".format(BOOT));
+print("kernel {0:s} {1:s}".format(kernel, parameter));
+print("initrd {0:s}".format(initrd));
+print("boot\n");
+
