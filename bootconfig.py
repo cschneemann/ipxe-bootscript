1afe4e4b673aafb0959f6e969e39d343d11002da
Christian Schneemann <schneemann@b1-systems.de>
2021-01-12 14:32:11 +0100

    fixed initrd in kernel line, take just filename, not whole path

--- bootconfig.py
+++ bootconfig.py
@@ -28,11 +28,12 @@ else:
 kernel = get_bootparam(BOOT, "kernel")
 initrd = get_bootparam(BOOT, "initrd")
 parameter = get_bootparam(BOOT, "parameter")
+initrd_name = initrd.split('/')[-1]
 
 print("Content-type: text/plain\n")
 print("#!ipxe\n");
 print("#booting option {0:s}\n".format(BOOT));
-print("kernel {0:s} initrd={1:s} {2:s}".format(kernel, initrd, parameter));
+print("kernel {0:s} initrd={1:s} {2:s}".format(kernel, initrd_name, parameter));
 print("initrd {0:s}".format(initrd));
 print("boot\n");
 
